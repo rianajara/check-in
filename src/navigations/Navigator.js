53ab15fbd631fe350e20425e3d30a3b8bd20eabb
rianajara <38594848+rianajara@users.noreply.github.com>
2021-03-27 21:25:22 -0700

    Revert "아키텍처 수정"
    This reverts commit e3f18d544a7b4eda6aa5c2fe324dd6c98009cca9.

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,37 +1,41 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-// import Welcome from '../screens/Welcome';
-// import Register from '../screens/Register';
-// import LoginHost from '../screens/LoginHost';
-// import RegisterHost from '../screens/RegisterHost';
-// import LoginAttendee from '../screens/LoginAttendee';
-// import RegisterAttendee from '../screens/RegisterAttendee';
-// import MainAttendee from '../screens/MainAttendee';
-// import MainHost from '../screens/MainHost';
-// import CreateEvent from '../screens/CreateEvent';
-// import ViewEvents from '../screens/ViewEvents';
-// import ViewEvent from '../screens/ViewEvent';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import Welcome from '../screens/Welcome';
+import Register from '../screens/Register';
+import LoginHost from '../screens/LoginHost';
+import RegisterHost from '../screens/RegisterHost';
+import LoginAttendee from '../screens/LoginAttendee';
+import RegisterAttendee from '../screens/RegisterAttendee';
+import MainAttendee from '../screens/MainAttendee';
+import MainHost from '../screens/MainHost';
+import CreateEvent from '../screens/CreateEvent';
+import ViewEvents from '../screens/ViewEvents';
+import ViewEvent from '../screens/ViewEvent';
+
+
+  
 
 const stackNavigatorOptions = {
-  headerShown: false,
-};
+    headerShown:false
+}
 
-const AppNavigator = createStackNavigator(
-  {
-    // Welcome:{screen:Welcome},
-    // Register:Register,
-    // LoginHost:LoginHost,
-    // LoginAttendee:LoginAttendee,
-    // RegisterHost:RegisterHost,
-    // RegisterAttendee:RegisterAttendee,
-    // MainAttendee:MainAttendee,
-    // MainHost:MainHost,
-    // CreateEvent:CreateEvent,
-    // ViewEvents:ViewEvents,
-    // ViewEvent:ViewEvent
-  },
-  {
-    defaultNavigationOptions: stackNavigatorOptions,
-  }
+const AppNavigator = createStackNavigator({
+    Welcome:{screen:Welcome},
+    Register:Register,
+    LoginHost:LoginHost,
+    LoginAttendee:LoginAttendee,
+    RegisterHost:RegisterHost,
+    RegisterAttendee:RegisterAttendee,
+    MainAttendee:MainAttendee,
+    MainHost:MainHost,
+    CreateEvent:CreateEvent,
+    ViewEvents:ViewEvents,
+    ViewEvent:ViewEvent
+},
+{
+    defaultNavigationOptions : stackNavigatorOptions
+}  
 );
 export default createAppContainer(AppNavigator);
+
+ 
--- src/screens/LoginAttendee.js
+++ src/screens/LoginAttendee.js
@@ -1,109 +1,112 @@
-import React, { useContext } from 'react';
-import {
-  Text,
-  View,
-  Image,
-  TextInput,
-  KeyboardAvoidingView,
-  StyleSheet,
-} from 'react-native';
+import React from 'react';
+import {Text, View, Image, TextInput,KeyboardAvoidingView, StyleSheet} from 'react-native';
 import { Button } from 'react-native-elements';
 import { Input } from 'react-native-elements';
 import Icon from '@expo/vector-icons/AntDesign';
-import AppContext from '@Components/AppContext';
-import UserConstants from '@Constants/User';
-import alert from '@Components/Alert';
-
-const img_top = require('@Images/image.png');
+import Firebase from '../components/Firebase'
+import * as firebase from 'firebase';
 
 const LoginAttendee = (props) => {
-  const globalState = useContext(AppContext);
-  const userType = globalState.auth.userType;
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const image = require('../images/image.png');
+    return (
+        <View style={styles.contentContainer}>
+            <Image source={image} style={styles.smallImage} />
+            <Text style={{
+                    fontSize: 20,
+                    fontFamily:'Bold',
+                    alignSelf: "center",
+                    marginTop:50
+                    
+                }}
+                >Attendee Sign In</Text>
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder='Student Email'
+                    leftIcon={
+                        <Icon
+                            name='mail'
+                            size={24}
+                            color='black'
+                            style={styles.icon}
+                        />
+                    }
+                    onChangeText={text => setEmail(text)}
+                    value={email}
+                />
+                <Input
+                    placeholder='Enter password'
+                    secureTextEntry={true}
+                    leftIcon={
+                        <Icon
+                            name='lock'
+                            size={24}
+                            color='black'
+                            style={styles.icon}
+                        />
+                    }
+                    onChangeText={text => setPassword(text)}
+                    value={password}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button style={styles.smallButton} title="Log In" onPress={() => signInWithEmailPassword(email,password,props)} />
+                <Button style={styles.smallButton} type="clear" title="Forgot Password" />
+                <Button style={styles.smallButton} title = "Dev" onPress ={()=>props.navigation.navigate('MainAttendee')}/>
+            </View>
+        </View>
+        
+    )
+}
 
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+const signInWithEmailPassword = async(email,password,props)=> {
+  //var email = "test@example.com";
+  //var password = "hunter2";
+  // [START auth_signin_password]
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      alert("Successfuly logged in.")
+      props.navigation.navigate('MainAttendee')
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert("Check your password and try again.")
+    });
+  // [END auth_signin_password]
+}
 
-  const dev = () => globalState.login();
-
-  const login = async () => {
-    if (email.length <= 0) return alert('이메일을 입력해주세요.');
-    else if (password.length <= 0) return alert('패스워드를 입력해주세요.');
-
-    globalState.changeLoading(true);
-    const user = await UserService.signIn(email, password, userType);
-    globalState.changeLoading(false);
-    if (!user) return alert('로그인 실패');
-    globalState.login({ user });
-  };
-
-  return (
-    <View style={styles.contentContainer}>
-      <Image source={img_top} style={styles.smallImage} />
-      <Text
-        style={{
-          fontSize: 20,
-          fontFamily: 'Bold',
-          alignSelf: 'center',
-          marginTop: 50,
-        }}>
-        Attendee Sign In
-      </Text>
-      <View style={styles.inputContainer}>
-        <Input
-          placeholder="Student Email"
-          leftIcon={
-            <Icon name="mail" size={24} color="black" style={styles.icon} />
-          }
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-        />
-        <Input
-          placeholder="Enter password"
-          secureTextEntry={true}
-          leftIcon={
-            <Icon name="lock" size={24} color="black" style={styles.icon} />
-          }
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button style={styles.smallButton} title="Log In" />
-        <Button
-          style={styles.smallButton}
-          type="clear"
-          title="Forgot Password"
-        />
-        <Button style={styles.smallButton} title="Dev" onPress={login} />
-      </View>
-    </View>
-  );
-};
 const styles = StyleSheet.create({
-  contentContainer: {
-    backgroundColor: '#fff7d5',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  smallImage: {
-    marginTop: 60,
-    width: 200,
-    height: 200,
-  },
-  inputContainer: {
-    width: '90%',
-    marginTop: 50,
-    justifyContent: 'space-evenly',
-  },
-  buttonContainer: {
-    justifyContent: 'space-around',
-    height: '40%',
-  },
-  smallButton: {},
-  icon: {
-    marginRight: 15,
-  },
-});
+    contentContainer: {
+        backgroundColor:"#fff7d5",
+        flex: 1,
+        alignItems: 'center',
+        justifyContent:'space-evenly'  
+    },
+    smallImage: {
+        marginTop:60,
+        width: 200,
+        height: 200,
+    },
+    inputContainer: {
+        width: '90%',
+        marginTop: 50,
+        justifyContent:'space-evenly',
+        
+    },
+    buttonContainer: {
+        justifyContent:'space-around',
+        height: '40%',
+    },
+    smallButton: {
+    },
+    icon: {
+        marginRight: 15
+    }
+})
 
 export default LoginAttendee;
