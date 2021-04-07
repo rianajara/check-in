df2fe3df1d5ed6b60ee66fc97f337fbe24f7bec7
TaylorBui94 <taylorbui1994@live.com>
2021-04-06 22:40:38 -0700

    viewaccount_attendee & host
    Added text file for it. The current data is dummy data and needs to be connected to firebase

--- src/screens/ViewAccount_Attendee.js
+++ src/screens/ViewAccount_Attendee.js
@@ -1,48 +1,191 @@
 import React from 'react';
-import {Text, View, Image, TextInput,KeyboardAvoidingView, StyleSheet, Platform} from 'react-native';
-import { Button , Input } from 'react-native-elements';
+import {
+	Text,
+	View,
+	Image,
+	TextInput,
+	KeyboardAvoidingView,
+	StyleSheet,
+} from 'react-native';
+import { Button } from 'react-native-elements';
+import { Input } from 'react-native-elements';
 import Icon from '@expo/vector-icons/AntDesign';
+import ViewAccount_Attendee from './ViewAccount_Attendee';
+import Firebase from '../components/Firebase';
+import * as firebase from 'firebase';
+import PopUpModal from '../components/PopUpModal';
+import InfoDropDown from '../components/InfoDropDown';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const ViewAttendee= (props) => {
-    return(
-        <View style={styles.contentContainer}>
-            <Text style={{
-                fontSize: 20,
-                fontFamily:'Bold',
-                alignSelf: "center",
-                marginTop:50
-            }}
-            >Attendee Account</Text>
+const viewAttendee = (props) => {
 
-        {/* Need to find way to get current user information on to input --- connect to user database for this */}
+	//connect to firebase somehow (?)
+	const firstName = 'First';
+	const lastName = 'Last';
+	const email = 'Test@email.com';
+	const major = 'Computer Science';
+	const yearLevel = 'Junior';
 
-            <Button style={styles.smallButton}  title="Modify Attendee" onPress ={()=>props.navigation.navigate('ModifyAttendee')}/>
-        </View>
-    )
-}
+	const switchPage = () => {
+		setTimeout(() => {
+			props.navigation.navigate('ViewAccount_Attendee');
+		}, 750);
+	};
+
+	return (
+      
+		//attendee information
+		<View style={styles.contentContainer} >
+			
+			<Text
+				style={{
+					fontSize: 20,
+					fontFamily: 'Bold',
+					alignSelf: 'center',
+					marginTop: 50,
+				}}>
+				 Attendee Information{' '}
+				 
+			</Text>
+			<ScrollView >
+
+				<View style={styles.textContainer}>
+
+					<Text style ={styles.biggerFont}> {'Name:'} </Text>
+
+
+
+					<Text styles = {styles.smallerFont}>
+							{'  '}
+           					<Icon //first name and last name
+               			 		name='user'
+                				size={20}
+               			   		color='black'
+               			    	style={styles.icon}
+							/>
+                    
+						{ ' '+ firstName + ' ' + lastName + '\n' }
+					</Text>
+
+
+					
+
+  
+
+
+					<Text style ={styles.biggerFont}> {'Email:'} </Text>
+
+			
+					<Text styles = {styles.smallerFont}>
+							{'  '}
+           					<Icon //email
+               			 		name='mail'
+                				size={20}
+               			   		color='black'
+               			    	style={styles.icon}
+							/>
+                    
+						{ ' '+ email + '\n' }
+					</Text>
+				
+			
+
+					<Text style ={styles.biggerFont}> {'Major:'} </Text>
+
+					<Text styles = {styles.smallerFont}>
+							{'  '}
+           					<Icon //major
+               			 		name='file-markdown'  
+                				size={20}
+               			   		color='black'
+               			    	style={styles.icon}
+							/>
+                    
+						{ ' '+ major + '\n' }
+					</Text>
+
+					<Text style ={styles.biggerFont}> {'Year Level:'} </Text>
+
+
+
+					<Text styles = {styles.smallerFont}>
+							{'  '}
+           					<Icon //year level
+               			 		name='book' 
+                				size={20}
+               			   		color='black'
+               			    	style={styles.icon}
+							/>
+                    
+						{ ' '+ yearLevel + '\n' }
+					</Text>
+
+            
+
+            	</View>
+			</ScrollView>
+
+			
+			<View style={styles.buttonContainer}>
+				<Button //modify attendee button
+					style={styles.smallButton}
+					title='Edit Info'
+					onPress ={()=>props.navigation.navigate('ModifyAttendee')}
+						
+					
+				/>
+			</View>
+		
+		</View>
+	);
+};
+
+
+
+
 
 const styles = StyleSheet.create({
-    contentContainer: {
-        backgroundColor:"#fff7d5",
-        flex: 1,
-        alignItems: 'center',
-        justifyContent:'space-evenly'  
-    },
-    inputContainer: {
-        width: '90%',
-        marginTop: 50,
-        justifyContent:'space-evenly',
-        
-    },
-    buttonContainer: {
-        justifyContent:'space-around',
-        height: '30%',
-    },
-    smallButton: {
-    },
-    icon: {
-        marginRight: 15
-    }
-})
+	contentContainer: {
+		backgroundColor: '#fff7d5',
+		flex: 1,
+		//alignItems: 'center',
+		justifyContent: 'space-evenly',
+	},
+	smallImage: {
+		marginTop: 60,
+		width: 200,
+		height: 200,
+	},
+	textContainer: {
 
-export default ViewAttendee;
+        alignItems: 'flex-start',
+        marginLeft: '10%',
+	
+
+        marginTop: 20,
+        justifyContent:'space-evenly',
+	},
+	buttonContainer: {
+        alignItems: 'center',
+		justifyContent: 'space-around',
+		height: '17%',
+	},
+	smallButton: {},
+	icon: {
+		marginRight: 15,
+	},
+
+	biggerFont:{
+		fontSize: 20,
+	},
+
+	smallerFont:{
+		fontSize: 15,
+		
+	},
+    
+});
+
+export default viewAttendee;
+
+
--- src/screens/ViewAccount_Host.js
+++ src/screens/ViewAccount_Host.js
@@ -1,51 +1,177 @@
 import React from 'react';
-import {Text, View, Image, TextInput,KeyboardAvoidingView, StyleSheet, Platform} from 'react-native';
+import {
+	Text,
+	View,
+	Image,
+	TextInput,
+	KeyboardAvoidingView,
+	StyleSheet,
+} from 'react-native';
 import { Button } from 'react-native-elements';
 import { Input } from 'react-native-elements';
 import Icon from '@expo/vector-icons/AntDesign';
+import ViewAccount_Attendee from './ViewAccount_Attendee';
+import Firebase from '../components/Firebase';
+import * as firebase from 'firebase';
+import PopUpModal from '../components/PopUpModal';
+import InfoDropDown from '../components/InfoDropDown';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const ViewHost= (props) => {
-    return(
-        <View style={styles.contentContainer}>
-            <Text style={{
-                fontSize: 20,
-                fontFamily:'Bold',
-                alignSelf: "center",
-                marginTop:50
-                
-            }}
-            >Host Account
-            </Text>
+const viewHost = (props) => {
+
+	//connect to firebase somehow (?)
+	const firstName = 'First';
+	const lastName = 'Last';
+	const email = 'Test@email.com';
+	const organization = 'org1'
+
+	const switchPage = () => {
+		setTimeout(() => {
+			props.navigation.navigate('ViewAccount_Attendee');
+		}, 750);
+	};
+
+	return (
+      
+		//attendee information
+		<View style={styles.contentContainer} >
+			
+			<Text
+				style={{
+					fontSize: 20,
+					fontFamily: 'Bold',
+					alignSelf: 'center',
+					marginTop: 50,
+				}}>
+				 Attendee Information{' '}
+				 
+			</Text>
+			<ScrollView >
+
+				<View style={styles.textContainer}>
+
+					<Text style ={styles.biggerFont}> {'Name:'} </Text>
+
+
+
+					<Text styles = {styles.smallerFont}>
+							{'  '}
+           					<Icon //user text
+               			 		name='user'
+                				size={20}
+               			   		color='black'
+               			    	style={styles.icon}
+							/>
+                    
+						{ ' '+ firstName + ' ' + lastName + '\n' }
+					</Text>
+
+
+					
+
+  
+
+
+					<Text style ={styles.biggerFont}> {'Email:'} </Text>
+
+			
+					<Text styles = {styles.smallerFont}>
+							{'  '}
+           					<Icon // email text
+               			 		name='mail'
+                				size={20}
+               			   		color='black'
+               			    	style={styles.icon}
+							/>
+                    
+						{ ' '+ email + '\n' }
+					</Text>
+				
+			
+
+					<Text style ={styles.biggerFont}> {'Organizations:'} </Text>
+
+					<Text styles = {styles.smallerFont}>
+							{'  '}
+           					<Icon // organization text
+               			 		name='addusergroup'  
+                				size={20}
+               			   		color='black'
+               			    	style={styles.icon}
+							/>
+                    
+						{ ' '+ organization + '\n' }
+					</Text>
+
+
+     
+
             
-            {/* Need to find way to get current user information on to input --- connect to user database for this */}
 
-            <Button style={styles.smallButton}  title="Modify Host"onPress ={()=>props.navigation.navigate('ModifyHost')}/>
-        </View>
-    )
-}
+            	</View>
+			</ScrollView>
+
+			
+			<View style={styles.buttonContainer}>
+				<Button //modify attendee button
+					style={styles.smallButton}
+					title='Edit Info'
+					onPress ={()=>props.navigation.navigate('ModifyAttendee')}
+						
+					
+				/>
+			</View>
+		
+		</View>
+	);
+};
+
+
+
+
 
 const styles = StyleSheet.create({
-    contentContainer: {
-        backgroundColor:"#fff7d5",
-        flex: 1,
-        alignItems: 'center',
-        justifyContent:'space-evenly'  
-    },
-    inputContainer: {
-        width: '90%',
-        marginTop: 50,
-        justifyContent:'space-evenly',
-        
-    },
-    buttonContainer: {
-        justifyContent:'space-around',
-        height: '30%',
-    },
-    smallButton: {
-    },
-    icon: {
-        marginRight: 15
-    }
-})
+	contentContainer: {
+		backgroundColor: '#fff7d5',
+		flex: 1,
+		//alignItems: 'center',
+		justifyContent: 'space-evenly',
+	},
+	smallImage: {
+		marginTop: 60,
+		width: 200,
+		height: 200,
+	},
+	textContainer: {
 
-export default ViewHost;
+        alignItems: 'flex-start',
+        marginLeft: '10%',
+	
+
+        marginTop: 20,
+        justifyContent:'space-evenly',
+	},
+	buttonContainer: {
+        alignItems: 'center',
+		justifyContent: 'space-around',
+		height: '17%',
+	},
+	smallButton: {},
+	icon: {
+		marginRight: 15,
+	},
+
+	biggerFont:{
+		fontSize: 20,
+	},
+
+	smallerFont:{
+		fontSize: 15,
+		
+	},
+    
+});
+
+export default viewHost;
+
+
