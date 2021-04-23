c4e7b5725cd0af3a42be95bdba0207b35d7a4f79
Lennox Scott <chrislennoxs@hotmail.com>
2021-04-23 00:28:41 -0700

    scan to db

--- src/screens/CameraScan.js
+++ src/screens/CameraScan.js
@@ -9,74 +9,83 @@ import Firebase from '../components/Firebase';
 const db = Firebase.firestore();
 db.settings({ timestampsInSnapshots: true });
 
-const CameraScan= (props) => {
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    const image = require('../images/image.png');
-    const [hasPermission, setHasPermission] = useState(null);
-    const [scanned, setScanned] = useState(false);
-    const { currentUser, setCurrentUser } = useContext(UserContext);
+const CameraScan = (props) => {
+	const eventInfo = props.navigation.state.params.eventInfo;
+	const [email, setEmail] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	const image = require('../images/image.png');
+	const [hasPermission, setHasPermission] = useState(null);
+	const [scanned, setScanned] = useState(false);
+	const { currentUser, setCurrentUser } = useContext(UserContext);
+	const [title, setTitle] = useState(
+		eventInfo[Object.keys(eventInfo)[0]]['Title']
+	);
+  
 
+	useEffect(() => {
+		(async () => {
+			const { status } = await BarCodeScanner.requestPermissionsAsync();
+			setHasPermission(status === 'granted');
+		})();
+	}, []);
 
-    useEffect(() => {
-        (async () => {
-          const { status } = await BarCodeScanner.requestPermissionsAsync();
-          setHasPermission(status === 'granted');
-        })();
-      }, []);
-
-      const addAttendee= () => { 
-        db.collection('OrgEvents')
-            .doc(currentUser['hostOrg'])
-            .collection('Events')
-            .doc(title)
-            .collection('Attendees')
-            .doc({data})
-            .set(
-                {
-                      'Attendees': ""
-                    },
-                { merge: true }
-            )
-      };
+	const addAttendee = (data) => {
     
-      const handleBarCodeScanned = ({ type, data }) => {
-        setScanned(true);
-        // alert(`${data}`);
-        console.warn({data});
-      };
-    
-      if (hasPermission === null) {
-        return <Text>Requesting for camera permission</Text>;
-      }
-      if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-      }
-    
-    return ( 
-        <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned, addAttendee}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
-  );
-}
+		db.collection('OrgEvents')
+			.doc(currentUser['hostOrg'])
+			.collection('Events')
+			.doc(title)
+			.collection('Attendees')
+			.doc(data)
+			.set(
+				{
+					Attendees: '',
+        }
+			);
+	};
 
+	const handleBarCodeScanned = ({ type, data }) => {
+		setScanned(true);
+		// alert(`${data}`);
+		console.warn(data);
+		addAttendee(data);
+	};
+
+	if (hasPermission === null) {
+		return <Text>Requesting for camera permission</Text>;
+	}
+	if (hasPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
+
+	return (
+		<View style={styles.container}>
+			<BarCodeScanner
+				onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+				style={StyleSheet.absoluteFillObject}
+			/>
+			{scanned && (
+				<Button
+					title={'Tap to Scan Again'}
+					onPress={() => setScanned(false)}
+				/>
+			)}
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#fff',
-    },
-    barCodeView: {
-      width: '100%', 
-      height: '50%', 
-      marginBottom: 40
-    },
-  });
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#fff',
+	},
+	barCodeView: {
+		width: '100%',
+		height: '50%',
+		marginBottom: 40,
+	},
+});
 
 export default CameraScan;
