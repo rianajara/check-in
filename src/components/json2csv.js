11ca1b43d5d27cfe4217808e9b04bbba1a8602a7
TaylorBui94 <taylorbui1994@live.com>
2021-04-29 13:48:01 -0700

    Update json2csv.js
    Removed the hardcode.

--- src/components/json2csv.js
+++ src/components/json2csv.js
@@ -1,22 +1,32 @@
 //reference : https://attacomsian.com/blog/nodejs-convert-json-to-csv
 
-// require json-2-csv module
-const converter = require('json-2-csv');
-const fs = require('fs');
+//Command to test it. Takes a json file and then creates a .csv with the json file name
+toCSV('infoList.json');
 
-// read JSON from a file
-const todos = JSON.parse(fs.readFileSync('infoList.json'));
+function toCSV(fileName) {
 
-// convert JSON array to CSV string
-converter.json2csv(todos, (err, csv) => {
-    if (err) {
-        throw err;
-    }
+    // require json-2-csv module
+    const converter = require('json-2-csv');
+    const fs = require('fs');
+
+
+    // read JSON from a file
+    const todos = JSON.parse(fs.readFileSync(fileName));
+
+    // convert JSON array to CSV string
+    converter.json2csv(todos, (err, csv) => {
+        if (err) {
+          throw err;
+       }
 
     // print CSV string
     console.log(csv);
 
+    //Removes the extension (.json) from the filename
+    const fileNameSpliced = fileName.replace(/\.[^/.]+$/, "");
+    
     // write CSV to a file
-    fs.writeFileSync('infoList.csv', csv);
+    fs.writeFileSync(fileNameSpliced + '.csv', csv);
     
 });
+}
