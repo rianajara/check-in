2212c64eeec2cedbc897789e20140091a9c38073
rianajara <38594848+rianajara@users.noreply.github.com>
2021-05-04 23:38:30 -0700

    Update EmailCSV.js

--- src/csv/EmailCSV.js
+++ src/csv/EmailCSV.js
@@ -1,12 +1,21 @@
 const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const fs = require("fs");
+pathToAttachment = `${__dirname}/demodata.csv`;
+attachment = fs.readFileSync(pathToAttachment).toString("base64");
 
 const msg = {
   to: 'rianajara@yahoo.com', // Change to your recipient
   from: 'csulbteamaxiom@gmail.com', // Change to your verified sender
   subject: 'Here is your event data',
   text: 'sending test data',
-  
+  attachments: [
+    {
+      content: attachment,
+      filename: "demodata.csv",
+      type: "application/csv",
+      disposition: "attachment"
+    }
+  ],
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 }
 sgMail
